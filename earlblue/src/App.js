cb72f916813cadce7dca3af5cbce50ba27023e8d
Aditya <adiiitya08@gmail.com>
2026-01-18 13:38:18 +0100

    added two img layer

--- earlblue/src/App.js
+++ earlblue/src/App.js
@@ -1,7 +1,13 @@
 import LandingPage from "./LandingPage";
-
+import TwoImageRow from "./TwoImage"; 
 
 export default function App() {
-  return <LandingPage />;
+  return (
+    <>
+      <LandingPage />
+      <TwoImageRow />
+    </>
+
+  );
 }
 
